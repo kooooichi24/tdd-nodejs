6e38ed3c9583fdf3c576fe0cbd63b3d567119929
古川広一 <ex.kouichi1224@gmail.com>
2021-03-06 20:22:43 +0900

    Add:  実装完了

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,4 +1,4 @@
-import {add} from '../src/index';
+import {add, subtract, multiply, divide} from '../src/index';
 
 describe('足し算のテスト', () => {
   describe('引数の要素数が 30 個以下の場合、加算結果を返す', () => {
@@ -47,10 +47,14 @@ describe('足し算のテスト', () => {
 
         expect(add(oneToThirty)).toBe(465);
       });
+
+      test('[]  を渡すと 0 を返す', () => {
+        expect(add([])).toBe(0);
+      });
     });
 
     describe('計算結果が 1000 を超える場合、合計ではなく「too big」と文字列が返す', () => {
-      test('計算結果が 1001 の場合、文字列「too big」を返す', () => {
+      test('[1,1000]を渡すと文字列「too big」を返す', () => {
         expect(add([1, 1000])).toBe('too big');
       });
     });
@@ -95,4 +99,337 @@ describe('足し算のテスト', () => {
       expect(() => add(oneToThityOne)).toThrow(Error);
     });
   });
+});
+
+describe('引き算のテスト', () => {
+  describe('引数の要素数が 30 個以下の場合、減算結果を返す', () => {
+    test('[1] を渡すと 1 を返す', () => {
+      expect(subtract([1])).toBe(1);
+    });
+
+    test('[3,1] を渡すと 2 を返す', () => {
+      expect(subtract([3, 1])).toBe(2);
+    });
+
+    test('[3,2,1] を渡すと 0 を返す', () => {
+      expect(subtract([3, 2, 1])).toBe(0);
+    });
+
+    test('[465,30,29,28,27,...,3,2] を渡すと 1 を返す', () => {
+      const argsLenIsThirty = [
+        465,
+        30,
+        29,
+        28,
+        27,
+        26,
+        25,
+        24,
+        23,
+        22,
+        21,
+        20,
+        19,
+        18,
+        17,
+        16,
+        15,
+        14,
+        13,
+        12,
+        11,
+        10,
+        9,
+        8,
+        7,
+        6,
+        5,
+        4,
+        3,
+        2,
+      ];
+
+      expect(subtract(argsLenIsThirty)).toBe(1);
+    });
+
+    test('[]  を渡すと 0 を返す', () => {
+      expect(subtract([])).toBe(0);
+    });
+  });
+  describe('引数の要素数が 31 個以上の場合、例外を返す', () => {
+    test('[465,30,29,28,27,...,3,2,1] を渡すと例外を返す', () => {
+      const argsLenIsThirtyOne = [
+        465,
+        30,
+        29,
+        28,
+        27,
+        26,
+        25,
+        24,
+        23,
+        22,
+        21,
+        20,
+        19,
+        18,
+        17,
+        16,
+        15,
+        14,
+        13,
+        12,
+        11,
+        10,
+        9,
+        8,
+        7,
+        6,
+        5,
+        4,
+        3,
+        2,
+        1,
+      ];
+
+      expect(() => subtract(argsLenIsThirtyOne)).toThrow(Error);
+    });
+  });
+  describe('計算結果が 負の場合、文字列「negative number」を返す', () => {
+    test('[3,10,3]を渡すと文字列「negative number」を返す', () => {
+      expect(subtract([3, 10, 3])).toBe('negative number');
+    });
+  });
+});
+
+describe('掛け算のテスト', () => {
+  describe('引数の要素数が 30 個以下の場合、減算結果を返す', () => {
+    test('[1] を渡すと 1 を返す', () => {
+      expect(multiply([1])).toBe(1);
+    });
+
+    test('[3,1] を渡すと 3 を返す', () => {
+      expect(multiply([3, 1])).toBe(3);
+    });
+
+    test('[3,2,1] を渡すと 6 を返す を渡すと 6 を返す', () => {
+      expect(multiply([3, 2, 1])).toBe(6);
+    });
+
+    test('[1,1,1,...,1] を渡すと 1 を返す', () => {
+      const argsLenIsThirty = [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+      ];
+
+      expect(multiply(argsLenIsThirty)).toBe(1);
+    });
+
+    test('[] を渡すと 0 を返す', () => {
+      expect(multiply([])).toBe(0);
+    });
+  });
+  describe('引数の要素数が 31 個以上の場合、例外を返す', () => {
+    test('[1,1,1,...,1] を渡すと例外を返す', () => {
+      const argsLenIsThirtyOne = [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+      ];
+
+      expect(() => multiply(argsLenIsThirtyOne)).toThrow(Error);
+    });
+  });
+  describe('計算結果が 1000を超える場合、文字列「big big number」を返す', () => {
+    test('[1,1,1001]を渡すと文字列「big big number」を返す', () => {
+      expect(multiply([1, 1, 1001])).toBe('big big number');
+    });
+  });
+});
+
+describe('割り算のテスト', () => {
+  describe('引数の要素数が 30 以下の場合、除算結果を返す', () => {
+    test('[1] を渡すと 1 を返す', () => {
+      expect(divide([1])).toBeCloseTo(1, 5);
+    });
+
+    test('[3,1] を渡すと 3 を返す', () => {
+      expect(divide([3, 1])).toBeCloseTo(3, 5);
+    });
+
+    test('[6,3,2] を渡すと 1 を返す', () => {
+      expect(divide([6, 3, 2])).toBeCloseTo(1, 5);
+    });
+
+    test('[1,1,1,...,1] を渡すと 1 を返す', () => {
+      const argsLenIsThirty = [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+      ];
+
+      expect(divide(argsLenIsThirty)).toBeCloseTo(1, 5);
+    });
+
+    test('[] を渡すと 0 を返す', () => {
+      expect(divide([])).toBeCloseTo(0, 5);
+    });
+
+    test('[0,1] を渡すと 0 を返す', () => {
+      expect(divide([0, 1])).toBeCloseTo(0, 5);
+    });
+  });
+
+  describe('ゼロ除算 の場合、正の無限大を返す', () => {
+    test('[1,0] を渡すと Number.POSITIVE_INFINITY を返す', () => {
+      expect(divide([1, 0])).toEqual(Number.POSITIVE_INFINITY);
+    });
+  });
+
+  describe('0/0 の場合、NaN を返す', () => {
+    test('[0,0] を渡すと NaN を返す', () => {
+      expect(divide([0, 0])).toEqual(NaN);
+    });
+  });
+
+  describe('引数の要素数が 31 以上の場合、例外を返す', () => {
+    const argsLenIsThirty = [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+    ];
+
+    expect(() => divide(argsLenIsThirty)).toThrow(Error);
+  });
+
+  // describe('除算結果は小数点 5 桁まで考慮する', () => {
+  //   test('[1,3] を渡すと 0.33333 を返す', () => {
+  //     expect(divide([1, 3])).toBeCloseTo(0.33333, 5);
+  //   });
+
+  //   test('[1,0.3] を渡すと 3.33333 を返す', () => {
+  //     expect(divide([1, 0.3])).toBeCloseTo(3.33333, 5);
+  //   });
+
+  //   test('[1,0.03] を渡すと 33.33333 を返す', () => {
+  //     expect(divide([1, 0.03])).toBeCloseTo(33.33333, 5);
+  //   });
+
+  //   // 失敗しちゃう、、
+  //   // Number型の精度に関してはどうすればいいんだろう？
+  //   test('[10000, 0.0000003] を渡すと 33333333333.33333 が返ってきて欲しい、、、', () => {
+  //     expect(divide([10000, 0.0000003])).toBeCloseTo(33333333333.33333, 5);
+  //   });
+  // });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@
  *
  * ただし下記条件を含む。
  * - 引数が数値のみの場合、加算結果を返す。
- * - 引数に文字列が含まれている場合、例外を返す。
  * - 引数の要素数が 31 個以上の場合、例外を返す。
  * - 加算結果が 1000 を超える場合、文字列「too big」を返す。
  *
@@ -18,9 +17,81 @@ export const add = (array: number[]): number | string | Error => {
     return acc + cur;
   }, 0);
 
-  if (result > 1000) {
+  if (result < 1000) {
+    return result;
+  } else {
     return 'too big';
+  }
+};
+
+/**
+ * 引き算メソッド。
+ *
+ * ただし下記条件を含む。
+ * - 引数が数値のみの場合、減算結果を返す。
+ * - 引数の要素数が 31 個以上の場合、例外を返す。
+ * - 加算結果が 負の場合、文字列「negative number」を返す。
+ *
+ * @param array 数値配列
+ */
+export const subtract = (array: number[]): number | string | Error => {
+  if (array.length > 30) {
+    throw new Error();
+  }
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  const result = array.reduce((acc, cur) => {
+    return acc - cur;
+  });
+
+  if (result < 0) {
+    return 'negative number';
   } else {
     return result;
   }
+};
+
+/**
+ * 掛け算メソッド。
+ *
+ * ただし下記条件を含む。
+ * - 引数が数値のみの場合、乗算結果を返す。
+ * - 引数の要素数が 31 個以上の場合、例外を返す。
+ * - 加算結果が 1000 を超える場合、文字列「big big number」を返す。
+ *
+ * @param array 数値配列
+ */
+export const multiply = (array: number[]): number | string | Error => {
+  if (array.length === 0) {
+    return 0;
+  }
+  if (array.length > 30) {
+    throw new Error();
+  }
+
+  const result = array.reduce((acc, cur) => {
+    return acc * cur;
+  });
+
+  if (result < 1000) {
+    return result;
+  } else {
+    return 'big big number';
+  }
+};
+
+export const divide = (array: number[]): number | Error => {
+  if (array.length === 0) {
+    return 0;
+  }
+  if (array.length > 30) {
+    throw new Error();
+  }
+
+  return array.reduce((acc, cur) => {
+    return acc / cur;
+  });
 };
